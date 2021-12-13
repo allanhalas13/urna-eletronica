e6d3d800ecb406b7735273095954985dcac510a7
Allan Halas <31524019+allanhalas13@users.noreply.github.com>
2021-12-13 14:18:23 -0300

    Create etapas.js

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Elon Musk',
+                partido: 'Tesla',
+                fotos:[
+                    {url: '.jpg', legenda: 'Vereador', small: false}
+                    ]
+            },
+            {
+                numero: '77222',
+                nome: 'Jeff',
+                partido: 'Amazon',
+                fotos:[
+                    {url:';jpg', legenda: 'Vereador'}
+                    ]
+            },
+            ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'99',
+                nome: 'Bill Gates',
+                partido: 'Microsoft',
+                vice: 'Cic',
+                fotos:[
+                    {url: '.jpg', legenda: 'Prefeito'},
+                    {url: '.jpg', legenda: 'Vice-Prefeito', small: true}
+                    ]
+            },
+            {
+                numero: '84',
+                nome: 'Steve',
+                partido: 'Apple',
+                vice: 'Zul',
+                fotos:[
+                    {url: '.jpg', legenda: 'Prefeito'},
+                    {url: '.jpg', legenda: 'Vice-Prefeito', small: true}
+                    ]
+            },
+        ]    
+    }
+];
